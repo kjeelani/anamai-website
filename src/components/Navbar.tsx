79f50dbac261854c96a1fdcb41526610c0624dfc
kjeelani <kaif.ajeelani@gmail.com>
2023-10-29 10:08:40 -0700

    Navbar changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -87,12 +87,9 @@ export default function Navbar(nvprops: NavbarProps) {
         <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
             <Flex alignItems={'center'}>
-                <Avatar
-                    size={'sm'}
-                    src={
-                    'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                    }
-                />
+                <Text fontSize="lg" fontWeight="bold" color="purple.800">
+                    Animai
+                </Text>
             </Flex>
             <IconButton
             size={'md'}
